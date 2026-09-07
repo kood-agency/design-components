f8954a7957a7829cabe8abc37e746578ee2e23a1
AlpoxDev <alpoxdev@gmail.com>
2026-09-07 17:39:26 +0900

    refactor(ui): 배포 컴포넌트를 ui 폴더로 분리

---
+++ src/components/ui/collapsible.tsx
@@ -0,0 +1,31 @@
+import { Collapsible as CollapsiblePrimitive } from "@base-ui/react/collapsible";
+import { cn } from "../../lib/utils";
+
+function Collapsible({ ...props }: CollapsiblePrimitive.Root.Props) {
+  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />;
+}
+
+function CollapsibleTrigger({ className, ...props }: CollapsiblePrimitive.Trigger.Props) {
+  return (
+    <CollapsiblePrimitive.Trigger
+      data-slot="collapsible-trigger"
+      className={cn("max-md:min-h-11 max-md:min-w-11", className)}
+      {...props}
+    />
+  );
+}
+
+function CollapsibleContent({ className, ...props }: CollapsiblePrimitive.Panel.Props) {
+  return (
+    <CollapsiblePrimitive.Panel
+      data-slot="collapsible-content"
+      className={cn(
+        "ease-standard overflow-hidden text-sm transition-opacity duration-(--duration-enter) data-ending-style:opacity-0 data-starting-style:opacity-0 motion-reduce:transition-none",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
